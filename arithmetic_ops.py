8353858dabd6bc7c6781924de1be47574a155de5
alaa-sayed-ai-expert-flow <168715269+alaa-sayed-ai-expert-flow@users.noreply.github.com>
2024-05-02 10:59:45 +0300

    Add files via upload

---
+++ arithmetic_ops.py
@@ -0,0 +1,22 @@
+num1 = float(input("Enter the First Number: "))
+num2 = float(input("Enter the Second Number: "))
+
+#addition
+
+print("{} + {} = ".format(num1, num2))
+print(num1 + num2)
+
+#subtraction
+
+print("{} - {} = ".format(num1, num2))
+print(num1 - num2)
+
+#multiplication
+
+print("{} * {} = ".format(num1, num2))
+print(num1 * num2)
+
+#division
+
+print("{} / {} = ".format(num1, num2))
+print(num1 / num2)
